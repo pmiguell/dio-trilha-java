3c12dbb83b3f4d030e6689ea06c85e5fe2ceca17
Pedro Miguel <pedromiguellitoo@gmail.com>
2024-10-01 10:40:13 -0300

    adding project Banco Digital

---
+++ banco-digital/src/Conta.java
@@ -0,0 +1,48 @@
+public abstract class Conta implements IConta {
+
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numeroConta;
+    protected double saldo;
+    protected Cliente cliente;
+
+    public Conta(Cliente cliente) {
+        this.agencia = AGENCIA_PADRAO;
+        this.numeroConta = SEQUENCIAL++;
+        this.cliente = cliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void sacar(double valor) {
+        saldo -= valor;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public void transferir(double valor, IConta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+
+    protected void imprimirInfosComuns() {
+        System.out.println(String.format("Titular: %s", cliente.getNome()));
+        System.out.println(String.format("Agência: %d", agencia));
+        System.out.println(String.format("Conta: %d", numeroConta));
+        System.out.println(String.format("Saldo: %.2f", saldo));
+    }
+}
